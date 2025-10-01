fd7879cc819da2ab93b89ea998f911ff0f4efe98
rabab alaa <161600491+Rabab17@users.noreply.github.com>
2025-10-01 14:18:08 +0300

    first commit

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,11 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  logger: {
+    updates: {
+      enabled: false,
+    },
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
